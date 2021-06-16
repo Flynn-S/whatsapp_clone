45607fbe18f7666763561c6314ff7c202a514ddc
Mihai Ivanov <mikee2988@yahoo.com>
2021-06-16 15:53:45 +0300

    code clean up

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,10 +1,8 @@
-// import atob from "atob";
 import { verifyJWT } from "./tools.js";
 import UserModel from "../models/user.js";
 
 export const jwtAuth = async (req, res, next) => {
   try {
-    // const token = req.headers.authorization.split(" ")[1];
     const token = req.cookies.accessToken;
     const decoded = await verifyJWT(token);
     const user = await UserModel.findOne({
@@ -20,27 +18,6 @@ export const jwtAuth = async (req, res, next) => {
     next(error);
   }
 };
-
-// export const basicAuthMiddleware = async (req, res, next) => {
-//   if (!req.headers.authorization) {
-//     const error = new Error("Please provide auth!");
-//     error.httpStatusCode = 401;
-//     next(error);
-//   } else {
-//     const decoded = atob(req.headers.authorization.split(" ")[1]);
-//     const [email, password] = decoded.split(":");
-
-//     const user = await AuthorSchema.checkCredentials(email, password);
-//     if (user) {
-//       req.user = user;
-//       next();
-//     } else {
-//       const error = new Error("Credentials are wrong");
-//       error.httpStatusCode = 401;
-//       next(error);
-//     }
-//   }
-// };
 
 export const adminOnly = (req, res, next) => {
   if (req.user && req.user.role === "Admin") {
