bae514ed91a3abc88ff6e38b3dc9d049d679cd48
Mihai Ivanov <mikee2988@yahoo.com>
2021-06-18 16:17:00 +0300

    added me-PUT

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import UserModel from '../models/user.js';
+import express from "express";
+import UserModel from "../models/user.js";
 // import ErrorResponse from "../lib/errorResponse.js";
-import { jwtAuth } from '../auth/index.js';
-import { auth, refreshJWT } from '../auth/tools.js';
-import passport from 'passport';
+import { jwtAuth } from "../auth/index.js";
+import { auth, refreshJWT } from "../auth/tools.js";
+import passport from "passport";
 const authRouter = express.Router();
 
-authRouter.post('/register', async (req, res, next) => {
+authRouter.post("/register", async (req, res, next) => {
   try {
     const newUser = await UserModel.create(req.body);
     const { _id } = newUser;
@@ -15,12 +15,12 @@ authRouter.post('/register', async (req, res, next) => {
       req.body.password
     );
     const tokens = await auth(user);
-    res.cookie('accessToken', tokens.accessToken, {
-      sameSite: 'lax',
+    res.cookie("accessToken", tokens.accessToken, {
+      sameSite: "lax",
       httpOnly: true,
     });
-    res.cookie('refreshToken', tokens.refreshToken, {
-      sameSite: 'lax',
+    res.cookie("refreshToken", tokens.refreshToken, {
+      sameSite: "lax",
       httpOnly: true,
     });
     res.status(201).send(_id);
@@ -29,17 +29,17 @@ authRouter.post('/register', async (req, res, next) => {
   }
 });
 
-authRouter.post('/login', async (req, res, next) => {
+authRouter.post("/login", async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const user = await UserModel.checkCredentials(email, password);
     const tokens = await auth(user);
-    res.cookie('accessToken', tokens.accessToken, {
-      sameSite: 'lax',
+    res.cookie("accessToken", tokens.accessToken, {
+      sameSite: "lax",
       httpOnly: true,
     });
-    res.cookie('refreshToken', tokens.refreshToken, {
-      sameSite: 'lax',
+    res.cookie("refreshToken", tokens.refreshToken, {
+      sameSite: "lax",
       httpOnly: true,
     });
     res.status(200).send();
@@ -49,31 +49,31 @@ authRouter.post('/login', async (req, res, next) => {
   }
 });
 
-authRouter.post('/logout', jwtAuth, async (req, res, next) => {
+authRouter.post("/logout", jwtAuth, async (req, res, next) => {
   try {
     req.user.refreshToken = null;
     await req.user.save();
-    res.clearCookie('accessToken');
-    res.clearCookie('refreshToken');
+    res.clearCookie("accessToken");
+    res.clearCookie("refreshToken");
     res.status(200).send();
   } catch (error) {
     next(error);
   }
 });
 
-authRouter.post('/refreshToken', async (req, res, next) => {
+authRouter.post("/refreshToken", async (req, res, next) => {
   const oldRefreshToken = req.body.refreshToken;
   if (!oldRefreshToken)
-    return next(new ErrorResponse('Refresh token is missing', 400));
+    return next(new ErrorResponse("Refresh token is missing", 400));
 
   try {
     const newTokens = await refreshJWT(oldRefreshToken);
-    res.cookie('accessToken', newTokens.accessToken, {
-      sameSite: 'lax',
+    res.cookie("accessToken", newTokens.accessToken, {
+      sameSite: "lax",
       httpOnly: true,
     });
-    res.cookie('refreshToken', newTokens.refreshToken, {
-      sameSite: 'lax',
+    res.cookie("refreshToken", newTokens.refreshToken, {
+      sameSite: "lax",
       httpOnly: true,
     });
 
@@ -87,24 +87,24 @@ authRouter.post('/refreshToken', async (req, res, next) => {
 // **************** OAuth ****************
 
 authRouter.get(
-  '/googleLogin',
-  passport.authenticate('google', { scope: ['profile', 'email'] })
+  "/googleLogin",
+  passport.authenticate("google", { scope: ["profile", "email"] })
 );
 authRouter.get(
-  '/auth/google/test',
-  passport.authenticate('google'),
+  "/auth/google/test",
+  passport.authenticate("google"),
   async (req, res, next) => {
     try {
-      res.cookie('accessToken', req.user.tokens.accessToken, {
-        sameSite: 'lax',
+      res.cookie("accessToken", req.user.tokens.accessToken, {
+        sameSite: "lax",
         httpOnly: true,
       });
 
-      res.cookie('refreshToken', req.user.tokens.refreshToken, {
-        sameSite: 'lax',
+      res.cookie("refreshToken", req.user.tokens.refreshToken, {
+        sameSite: "lax",
         httpOnly: true,
       });
-      res.status(200).redirect('http://localhost:3000');
+      res.status(200).redirect("http://localhost:3000");
     } catch (error) {
       next(error);
     }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,11 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import UserModel from '../models/user.js';
-import RoomModel from '../models/rooms.js';
+import UserModel from "../models/user.js";
+import RoomModel from "../models/rooms.js";
 
 const router = Router();
 
-router.get('/me', async (req, res, next) => {
+router.get("/me", async (req, res, next) => {
   try {
     res.send(req.user);
   } catch (error) {
@@ -14,12 +14,26 @@ router.get('/me', async (req, res, next) => {
   }
 });
 
-router.get('/', async (req, res, next) => {
+router.put("/me", async (req, res, next) => {
+  try {
+    const updates = Object.keys(req.body);
+
+    updates.forEach((u) => (req.user[u] = req.body[u]));
+
+    await req.user.save();
+
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/", async (req, res, next) => {
   try {
     const query = req.query.email;
 
     const findUser = await UserModel.find({
-      email: { $regex: new RegExp(query, 'i') },
+      email: { $regex: new RegExp(query, "i") },
     }).sort({ email: 1 });
     res.send(findUser);
   } catch (error) {
@@ -29,13 +43,13 @@ router.get('/', async (req, res, next) => {
 });
 // router.get("/:id", async (req, res, next) => {});
 
-router.get('/rooms', async (req, res, next) => {
+router.get("/rooms", async (req, res, next) => {
   try {
     const id = req.user._id;
 
     // if (id === ) return
 
-    const rooms = await RoomModel.find({ usersId: id }).populate('usersId');
+    const rooms = await RoomModel.find({ usersId: id }).populate("usersId");
 
     res.send(rooms);
   } catch (error) {
